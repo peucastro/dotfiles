4492e25322f05b3fa6d1e8652941b5ed9c8f5fd5
Pedro Castro <falecompedroac@gmail.com>
2025-03-30 16:49:54 +0100

    lualine

---
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    lazy = false,
+    config = {
+      options = {
+        theme = 'gruvbox'
+      }
+    },
+  }
+}
