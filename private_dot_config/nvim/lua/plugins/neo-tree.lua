098d5a4344cf1aab04c13dc733b7b311e049afca
Pedro Castro <falecompedroac@gmail.com>
2025-03-30 15:09:29 +0100

    nvim telescope

--- private_dot_config/nvim/lua/plugins/neo-tree.lua
+++ private_dot_config/nvim/lua/plugins/neo-tree.lua
@@ -4,13 +4,11 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
+      "MunifTanjim/nui.nvim"
     },
     lazy = false,
     opts = {
-      window = {
-        position = "right",
-      },
+      window = { position = "right" },
     },
   }
 }
