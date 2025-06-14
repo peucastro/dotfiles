c8d4205362908f38db8ca338e4f97516b7fcc7ed
Pedro Castro <falecompedroac@gmail.com>
2025-06-14 19:41:10 +0100

    ddcutil + scripts for managing brightness

---
+++ private_dot_config/hypr/scripts/executable_hypridle-dim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TMPFILE="$HOME/.config/hypr/.brightness_backup"
+
+mkdir -p "$(dirname "$TMPFILE")"
+
+brightnessctl -s set 10
+
+>"$TMPFILE"
+for id in $(ddcutil detect | grep -E '^Display [0-9]+' | awk '{print $2}'); do
+  current=$(sudo ddcutil --display="$id" getvcp 10 2>/dev/null | awk -F 'current value = |, max' '{print $2}' | tr -d ' ')
+  if [[ "$current" =~ ^[0-9]+$ ]]; then
+    echo "$id $current" >>"$TMPFILE"
+    sudo ddcutil --display="$id" setvcp 10 10
+  fi
+done
