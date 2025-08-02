337ff64a08ea0b21e4642db6418b02658a61d09b
Pedro Castro <falecompedroac@gmail.com>
2025-08-02 18:46:42 +0100

    disko config

--- flake.nix
+++ flake.nix
@@ -3,6 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
