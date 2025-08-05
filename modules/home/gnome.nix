3f7736a65edde929a35918369763a39bf9aa0136
pedro castro <falecompedroac@gmail.com>
2025-08-05 08:45:02 +0100

    configuring gnome

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -1,5 +1,66 @@
-{
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.caffeine
+    gnomeExtensions.clipboard-indicator
+    gnomeExtensions.vitals
+    gnomeExtensions.impatience
+    gnomeExtensions.alphabetical-app-grid
+  ];
+
   dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "firefox.desktop"
+        "bitwarden.desktop"
+        "org.gnome.Calendar.desktop"
+        "obsidian.desktop"
+        "spotify.desktop"
+        "slack.desktop"
+        "webcord.desktop"
+        "code.desktop"
+        "idea-ultimate.desktop"
+        "com.mitchellh.ghostty.desktop"
+        "startcenter.desktop"
+        "org.gnome.Nautilus.desktop"
+      ];
+      enabled-extensions = [
+        "dash-to-dock@micxgx.gmail.com"
+        "caffeine@patapon.info"
+        "clipboard-indicator@tudmotu.com"
+        "Vitals@CoreCoding.com"
+        "impatience@gfxmonk.net"
+        "AlphabeticalAppGrid@stuarthayhurst"
+      ];
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      show-battery-percentage = true;
+    };
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [
+        "System"
+        "Utilities"
+        "Office"
+      ];
+    };
+    "org/gnome/desktop/app-folders/folders/Office" = {
+      name = "Office";
+      apps = ["base.desktop" "calc.desktop" "draw.desktop" "impress.desktop" "math.desktop" "writer.desktop"];
+    };
+    "org/gnome/desktop/calendar" = {
+      show-weekday = true;
+    };
+    "org/gnome/shell/app-switcher" = {
+      current-workspace-only = true;
+    };
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
+      night-light-schedule-automatic = true;
+    };
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "list-view";
+    };
     "org/gnome/TextEditor" = {
       highlight-current-line = true;
       indent-style = "space";
@@ -7,9 +68,6 @@
       show-right-margin = false;
       style-scheme = "builder-dark";
       wrap-text = false;
-    };
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
     };
   };
 }
