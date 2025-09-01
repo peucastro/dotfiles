e83cc57c8f02aba0fef75a30f83556241c11c8a9
Pedro Castro <falecompedroac@gmail.com>
2025-09-01 19:49:44 +0100

    rebuild(yoga): generation 4, NixOS 25.05.20250831.b4c2c57 with Linux Kernel 6.12.44

---
+++ modules/core/xdg.nix
@@ -0,0 +1,9 @@
+{
+  xdg.mime.defaultApplications = {
+    "default-web-browser" = "firefox.desktop";
+    "image/png" = "loupe.desktop";
+    "image/jpeg" = "loupe.desktop";
+    "application/pdf" = "evince.desktop";
+    "text/plain" = "g-edit.desktop";
+  };
+}
