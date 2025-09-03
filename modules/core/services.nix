53eadaf075c932981728a43304e0b6684cd03256
Pedro Castro <falecompedroac@gmail.com>
2025-09-03 13:57:14 +0100

    fix: xdg portal config

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -11,14 +11,6 @@
         };
       };
     };
-
-    xdg-desktop-portal = {
-      enable = true;
-      wlr.enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-      ];
-    };
   };
 
   users.users.greeter = {
--- modules/core/xdg.nix
+++ modules/core/xdg.nix
@@ -1,4 +1,12 @@
-{
+{pkgs, ...}: {
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
