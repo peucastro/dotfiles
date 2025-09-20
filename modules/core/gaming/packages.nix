5ef6ad68a05955669fa3598777e482c0e762b9b7
Pedro Castro <falecompedroac@gmail.com>
2025-09-20 19:18:00 +0100

    rebuild(yoga): generation 11, NixOS 25.11.20250916.8d4ddb1 with Linux Kernel 6.12.47

--- modules/core/gaming/packages.nix
+++ modules/core/gaming/packages.nix
@@ -6,9 +6,11 @@
   environment.systemPackages = [
     pkgs.protonup-qt
     pkgs.mangohud
+    pkgs.lutris
+    pkgs.heroic
   ];
 
   environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/${user.login}/.steam/root/compatibilitytools.d";
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/${user.login}/.local/share/Steam/compatibilitytools.d";
   };
 }
