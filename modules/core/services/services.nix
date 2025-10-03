19489b7bd62e163d94b038192fb48150068945e3
Pedro Castro <falecompedroac@gmail.com>
2025-10-03 10:21:00 +0100

    rebuild(yoga): generation 24, NixOS 25.11.20250924.e643668 with Linux Kernel 6.12.48

--- modules/core/services/services.nix
+++ modules/core/services/services.nix
@@ -1,6 +1,9 @@
-{
+{pkgs, ...}: {
   services = {
     gnome.gnome-keyring.enable = true;
     power-profiles-daemon.enable = true;
   };
+
+  programs.seahorse.enable = true;
+  environment.systemPackages = [pkgs.libsecret];
 }
