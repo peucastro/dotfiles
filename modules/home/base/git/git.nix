04074cd372dd8081fef7cc0522c7f9f64d955848
Pedro Castro <falecompedroac@gmail.com>
2025-11-06 00:50:02 +0000

    rebuild(yoga): generation 33, NixOS 25.11.20251031.2fb006b with Linux Kernel 6.12.56

--- modules/home/base/git/git.nix
+++ modules/home/base/git/git.nix
@@ -1,31 +1,31 @@
 {user, ...}: {
   programs.git = {
     enable = true;
-
-    userName = user.displayName;
-    userEmail = user.email;
-
-    extraConfig = {
+    settings = {
+      user = {
+        name = user.displayName;
+        email = user.email;
+      };
       init.defaultBranch = "main";
       pull.ff = "only";
       color.ui = true;
       core.excludesFile = "/home/${user.login}/.config/git/.gitignore";
     };
-
     signing = {
       format = "ssh";
       key = "/home/${user.login}/.ssh/id_ed25519.pub";
       signByDefault = true;
     };
+  };
 
-    delta = {
-      enable = true;
-      options = {
-        line-numbers = true;
-        side-by-side = false;
-        diff-so-fancy = true;
-        navigate = true;
-      };
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      line-numbers = true;
+      side-by-side = false;
+      diff-so-fancy = true;
+      navigate = true;
     };
   };
 }
