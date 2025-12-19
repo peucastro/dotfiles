172ae2bf124ffd3bf11191547a6d202162c49f75
Pedro Castro <falecompedroac@gmail.com>
2025-12-19 23:22:25 +0000

    rebuild(yoga): generation 64, NixOS 26.05.20251211.2fbfb1d with Linux Kernel 6.12.61

--- hosts/yoga/state-configuration.nix
+++ hosts/yoga/state-configuration.nix
@@ -1,5 +1,5 @@
 {user, ...}: let
-  stateVersion = "25.05";
+  stateVersion = "25.11";
 in {
   system.stateVersion = stateVersion;
   home-manager.users.${user.login}.home.stateVersion = stateVersion;
