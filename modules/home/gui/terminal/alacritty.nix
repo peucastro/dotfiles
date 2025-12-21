13bcec2a19dce9b209221809518daecfa060516b
Pedro Castro <falecompedroac@gmail.com>
2025-12-21 10:35:15 +0000

    rebuild(yoga): generation 66, NixOS 26.05.20251211.2fbfb1d with Linux Kernel 6.12.61

--- modules/home/gui/terminal/alacritty.nix
+++ modules/home/gui/terminal/alacritty.nix
@@ -1,4 +1,6 @@
-{
+let
+  inherit (import ../desktop/colors.nix) colors;
+in {
   programs.alacritty = {
     enable = true;
     settings = {
@@ -8,7 +10,6 @@
           x = 8;
           y = 8;
         };
-        # decorations = "None";
         blur = true;
         startup_mode = "Maximized";
       };
@@ -20,11 +21,89 @@
         };
         size = 13.5;
       };
-      # colors = { ... }
+      colors = {
+        primary = {
+          background = "#${colors.background-dim}";
+          foreground = "#${colors.white}";
+          dim_foreground = "#${colors.foreground-dim}";
+          bright_foreground = "#${colors.foreground-alt}";
+        };
+        cursor = {
+          text = "#${colors.background}";
+          cursor = "#${colors.focused}";
+        };
+        vi_mode_cursor = {
+          text = "#${colors.background}";
+          cursor = "#${colors.unfocused}";
+        };
+        search = {
+          matches = {
+            foreground = "#${colors.background}";
+            background = "#${colors.red}";
+          };
+          focused_match = {
+            foreground = "#${colors.background}";
+            background = "#${colors.yellow}";
+          };
+        };
+        hints = {
+          start = {
+            foreground = "#${colors.background}";
+            background = "#${colors.yellow}";
+          };
+          end = {
+            foreground = "#${colors.background}";
+            background = "#${colors.red}";
+          };
+        };
+        line_indicator = {
+          foreground = "#${colors.orange}";
+          background = "#${colors.background-alt}";
+        };
+        footer_bar = {
+          foreground = "#${colors.background}";
+          background = "#${colors.foreground}";
+        };
+        selection = {
+          text = "#${colors.background}";
+          background = "#${colors.selection}";
+        };
+        normal = {
+          black = "#${colors.black}";
+          red = "#${colors.red}";
+          green = "#${colors.green}";
+          yellow = "#${colors.yellow}";
+          blue = "#${colors.blue}";
+          magenta = "#${colors.magenta}";
+          cyan = "#${colors.cyan}";
+          white = "#${colors.white}";
+        };
+        bright = {
+          black = "#${colors.gray}";
+          red = "#${colors.orange}";
+          green = "#${colors.green}";
+          yellow = "#${colors.yellow}";
+          blue = "#${colors.blue}";
+          magenta = "#${colors.magenta}";
+          cyan = "#${colors.cyan}";
+          white = "#${colors.foreground}";
+        };
+        dim = {
+          black = "#${colors.background-dim}";
+          red = "#${colors.unfocused}";
+          green = "#${colors.green}";
+          yellow = "#${colors.selection}";
+          blue = "#${colors.blue}";
+          magenta = "#${colors.magenta}";
+          cyan = "#${colors.cyan}";
+          white = "#${colors.gray}";
+        };
+        transparent_background_colors = true;
+      };
       bell = {
         animation = "EaseOutExpo";
         duration = 80;
-        color = "#ffb86c";
+        color = "#${colors.orange}";
       };
       cursor = {
         style = {
