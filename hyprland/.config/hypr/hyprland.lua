16b6404ae02f6c83e6e41c235b154e726c23c7bc
Pedro Castro <falecompedroac@gmail.com>
2026-06-06 18:23:37 +0100

    updated display config

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -8,8 +8,14 @@
 hl.monitor({
     output   = "eDP-1",
     mode     = "2880x1800@120Hz",
-    position = "auto",
+    position = "0x0",
     scale    = "1.5",
+})
+hl.monitor({
+    output   = "HDMI-A-1",
+    mode     = "1920x1080@100Hz",
+    position = "1920x0",
+    scale    = "1.0",
 })
 hl.monitor({
     output   = "",
